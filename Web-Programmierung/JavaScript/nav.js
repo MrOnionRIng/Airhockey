e418408dea73a8fc3cb205b646b588ad02470217
aaurich <a.aurich@ostfalia.de>
2018-10-15 22:15:22 +0200

    Navbar auf Impressum

--- Web-Programmierung/JavaScript/nav.js
+++
@@ -1,6 +0,0 @@
-document.write("<div class=\"navbar\">");
-document.write("<a class=\"active\" href=\"index.html\">Home</a>");
-document.write("<a href=\"#news\">News</a>");
-document.write("<a href=\"HTML/impressum.html\">Contact</a>");
-document.write("<a href=\"#about\">About</a>");
-document.write("</div>");
